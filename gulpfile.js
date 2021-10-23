f057a1305dcb968d04963c3eb521b8df82c983cc
mohammed-taysser <mohamedtaysser112@yahoo.com>
2021-10-23 13:37:59 +0200

    finish home page- restructre project

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,71 @@
 const gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
     autoprefixer = require('gulp-autoprefixer'),
     babel = require('gulp-babel'),
-    sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass')(require('sass')),
+    concat = require('gulp-concat'),
     del = require('del'),
     pug = require('gulp-pug'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass')(require('sass')),
+    uglify = require('gulp-uglify'),
     paths = {
         html: {
             src: 'src/html/pages/*.pug',
-            dest: 'dist/html'
+            dest: 'dist/html',
         },
-        css: {
-            src: ['src/css/**/*.css', 'src/css/*.scss'],
-            dest: 'dist/css/'
+        scss: {
+            src: 'src/css/*.scss',
+            dest: 'dist/css/',
         },
-        js: {
-            src: 'src/js/**/*.js',
-            dest: 'dist/js/'
+        javascript: {
+            libs: 'src/js/libs/*.js',
+            build: {
+                src: 'src/js/*.js',
+                dest: 'dist/javascript/build/',
+            },
+            demo: {
+                src: 'src/js/*.js',
+                dest: 'dist/javascript/',
+            },
         },
         fonts: {
             src: 'src/fonts/**',
-            dest: 'dist/fonts/'
+            dest: 'dist/fonts/',
         },
         images: {
             src: 'src/images/**',
-            dest: 'dist/images/'
-        }
+            dest: 'dist/images/',
+        },
     };
 function clean() {
     'use strict';
-    return del(['dist/**', '!dist']);
+    return del([
+        'dist/**', '!dist',
+    ]);
 }
 
-function un_minified_css_task() {
-    'use strict';
-    return gulp.src(paths.css.src)
-        .pipe(sourcemaps.init())
-        .pipe(autoprefixer())
-        .pipe(sass().on('error', sass.logError))
-        // .pipe(csso())
-        .pipe(concat('style.css'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest));
-}
 
-function minified_css_task() {
+function scss_task() {
     'use strict';
-    return gulp.src(paths.css.src)
+    return gulp.src(paths.scss.src)
         .pipe(sourcemaps.init())
         .pipe(autoprefixer())
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(concat('style.min.css'))
+        .pipe(rename(function (path) {path.extname = '.min.css';}))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest));
+        .pipe(gulp.dest(paths.scss.dest));
 }
+
+// function demo_scss_task() {
+//     'use strict';
+//     return gulp.src(paths.scss.src)
+//         .pipe(sourcemaps.init())
+//         .pipe(autoprefixer())
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(rename(function (path) {path.extname = '.css';}))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest(paths.scss.dest));
+// }
 
 function html_task() {
     'use strict';
@@ -71,25 +81,56 @@ function fonts_task() {
         .pipe(gulp.dest(paths.fonts.dest));
 }
 
-function un_minified_javascript_task() {
-    'use strict';
-    return gulp.src(paths.js.src)
-        .pipe(sourcemaps.init())
-        .pipe(babel({ presets: ['@babel/env'] }))
-        .pipe(concat('script.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.js.dest));
-}
+// function demo_js_task() {
+//     'use strict';
+//     return gulp.src(paths.javascript.demo.src)
+//         .pipe(sourcemaps.init())
+//         .pipe(babel({ presets: ['@babel/env'] }))
 
-function minified_javascript_task() {
+//         // .pipe(concat('script.js'))
+//         .pipe(rename(function (path) {
+//             if (path.basename.slice(-3) === 'min'){
+//                 path.basename = path.basename.slice(0, -4);
+//             }
+//         }))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest(paths.javascript.demo.dest));
+// }
+// function demo_libs_js_task() {
+//     'use strict';
+//     return gulp.src(paths.javascript.libs)
+//         .pipe(sourcemaps.init())
+//         .pipe(babel({ presets: ['@babel/env'] }))
+//         .pipe(concat('libs.js'))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest(paths.javascript.demo.dest));
+// }
+function build_js_task() {
     'use strict';
-    return gulp.src(paths.js.src)
+    return gulp.src(paths.javascript.build.src)
         .pipe(sourcemaps.init())
         .pipe(babel({ presets: ['@babel/env'] }))
-        .pipe(concat('script.min.js'))
+
+        // .pipe(concat('scripts.min.js'))
+        .pipe(rename(function (path) {
+            if (path.basename.slice(-3) !== 'min') {
+                path.extname = '.min.js';
+            }
+        }))
+
+        // .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.js.dest));
+        .pipe(gulp.dest(paths.javascript.build.dest));
+}
+function build_libs_js_task() {
+    'use strict';
+    return gulp.src(paths.javascript.libs)
+        .pipe(sourcemaps.init())
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.javascript.build.dest));
 }
 
 function images_task() {
@@ -100,24 +141,27 @@ function images_task() {
 
 function watch_fun() {
     'use strict';
-    gulp.watch(paths.html.src, html_task);
-    gulp.watch(paths.css.src, un_minified_css_task);
-    gulp.watch(paths.css.src, minified_css_task);
-    gulp.watch(paths.js.src, un_minified_javascript_task);
-    gulp.watch(paths.js.src, minified_javascript_task);
+    gulp.watch('src/html/**/*.pug', html_task);
+    // gulp.watch(paths.javascript.demo.src, demo_js_task);
+    // gulp.watch(paths.javascript.libs, demo_libs_js_task);
+    gulp.watch('src/js/**/*.js', build_js_task);
+    gulp.watch('src/js/**/*.js', build_libs_js_task);
     gulp.watch(paths.fonts.src, fonts_task);
+    gulp.watch('src/css/**/*.scss', scss_task);
+    // gulp.watch(paths.scss.demo.src, demo_scss_task);
     gulp.watch(paths.images.src, images_task);
 }
 
 
-exports.default = gulp.series(
-    clean,
+exports.default = gulp.series(clean,
     html_task,
-    un_minified_css_task,
-    minified_css_task,
-    un_minified_javascript_task,
-    minified_javascript_task,
+    build_js_task,
+    build_libs_js_task,
+    // demo_js_task,
+    // demo_libs_js_task,
     fonts_task,
     images_task,
-    gulp.parallel(watch_fun)
+    scss_task,
+    // demo_scss_task,
+    gulp.parallel(watch_fun),
 );
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,116 +1,100 @@
-$(function () {
-	'use strict';
 
-	bsCustomFileInput.init();
+// Toggle menu on navbar
+const close_button = document.getElementById('js-menu-toggle'),
+    menu_links = document.querySelector('#main-links ul');
 
-	$('[data-toggle="tooltip"]').tooltip();
-	$('[data-toggle="popover"]').popover();
+close_button.onclick = function (event) {
+    'use strict';
+    this.classList.toggle('open');
+    menu_links.classList.toggle('open');
+    event.stopPropagation();
+};
 
-	$('select').niceSelect();
+menu_links.onclick = function (e) {
+    'use strict';
+    e.stopPropagation();
+};
 
-    $(".offers .owl-carousel").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        dots:false,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            },
-            1200:{
-            	items:4
-			}
+document.addEventListener('click', event => {
+    'use strict';
+    if (event.target !== menu_links && event.target !== close_button) {
+        if (menu_links.classList.contains('open')) {
+            menu_links.classList.toggle('open');
+            close_button.classList.toggle('open');
         }
-    });
-	$(".specialises .owl-carousel").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-		autoplayTimeout:5500,
-        dots:false,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            },
-            1200:{
-            	items:4
-			}
-        }
-    });
-	$(".doctor-detail .owl-carousel").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-		autoplayTimeout:5500,
-        dots:false,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            },
-            1200:{
-            	items:4
-			}
-        }
-    });
-
-	// header carousel
-	let header_carousel = setInterval(function (){
-		let random_image_number = Math.floor(Math.random() * 5) + 1, header = $('header .header');
-		header.fadeOut(1000);
-		setTimeout(function (){
-			header.css('background-image', `url('images/header/header-` + random_image_number + `.jpg')`);
-		}, 1000);
-		setTimeout(function (){
-			header.fadeIn(1000);
-		}, 1100);
-	}, 5000);
-
-    $('.angle-down a').on('click', function (){
-        document.querySelector('.offers').scrollIntoView({behavior: 'smooth'});
-    });
-
-    // rotate filter svg on click
-    $('.doctors .accordion ul li').on('click', function (){
-        $('.doctors .accordion ul li .arrow').removeClass('rotate--90');
-        $(this).children('.arrow').toggleClass('rotate--90');
-    });
-
-
-	// show current year on footer
-	$('#current-year').text(new Date().getFullYear());
-
-	// replace email input with tel if start by number
-    $('.login-form #email-input').on('keyup', function (){
-        if(!isNaN($(this).val()[0])){
-            this.type = 'tel';
-        }
-        else {
-            this.type = 'email';
-        }
-    });
-
+    }
 });
+
+function setWindowHeight() {
+    'use strict';
+    const window_height = window.innerHeight,
+        nav_height = document.querySelector('nav').clientHeight;
+    document.getElementById('js-header').style.height = `${window_height - nav_height}px`;
+}
+
+window.addEventListener('resize', setWindowHeight, false);
+setWindowHeight();
+
+// Header carousel
+// Const header = document.querySelector('header');
+// SetInterval(function () {
+//     'use strict';
+//     Const random_image_number = Math.floor(Math.random() * 5) + 1;
+//     Header.style.opacity = 0;
+//     SetTimeout(function () {
+//         Header.style('background-image', `url('../images/header/header-${ random_image_number }.jpg')`);
+//     }, 1000);
+//     SetTimeout(function () {
+//         Header.style.opacity = 1;
+//     }, 1100);
+// }, 5000);
+
+document.querySelector('.angle-down a').onclick = function () {
+    'use strict';
+    document.querySelector('.offers').scrollIntoView({ behavior: 'smooth' });
+};
+
+// // Rotate filter svg on click
+// Document.querySelector('.doctors .accordion ul li').on('click', function () {
+//     'use strict';
+//     Document.querySelector('.doctors .accordion ul li .arrow').removeClass('rotate--90');
+//     This.children('.arrow').toggleClass('rotate--90');
+// });
+
+
+// // Show current year on footer
+const current_year = document.getElementById('js-current-year');
+current_year.textContent = new Date().getFullYear().toString();
+
+// Replace email input with tel if start by number
+// Document.querySelector('.login-form #email-input').onKeyup = function () {
+//     'use strict';
+//     If (!isNaN(this.value[0])) {
+//         This.type = 'tel';
+//     } else {
+//         This.type = 'email';
+//     }
+// };
+
+NiceSelect.bind(document.getElementById('js-city-select'));
+NiceSelect.bind(document.getElementById('js-specialist-select'));
+NiceSelect.bind(document.getElementById('js-area-select'));
+
+
+const options = {
+    type: 'carousel',
+    perView: 4,
+    autoplay: 5000,
+    breakpoints: {
+        992: {
+            perView: 3,
+        },
+        768: {
+            perView: 2,
+        },
+        567: {
+            perView: 1,
+        },
+    },
+}, offers = new Glide('#offers', options).mount()
+    , specialiies = new Glide('#specialiies', options).mount();
